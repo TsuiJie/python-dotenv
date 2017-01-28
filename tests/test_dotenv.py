6d37523984e09ca7ee75cfe3f7adf1492fe517cf
jndion2014 <jndion2014@gmail.com>
2017-01-28 22:50:27 +0800

    basic read capacity

---
+++ tests/test_dotenv.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+import types
+import dotenv
+
+try:
+    import mock
+except ImportError:
+    import unittest.mock as mock
+
+
+class TestDotEnv(unittest.TestCase):
+
+    def setUp(self):
+        file = open('.env', 'w')
+        file.write('TEST=ok')
+        file.close()
+        pass
+
+    def test_case(self):
+        env = dotenv.DotEnv()
+        env.all()
+        env.dump()
+        env.get('TEST')
+        env.has('TEST')
+        pass
